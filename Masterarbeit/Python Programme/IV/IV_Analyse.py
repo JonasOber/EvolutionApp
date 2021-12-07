a3f617c9b70cfbe04f8ffd53ad4d3dc78199080d
JonasOber <88477114+JonasOber@users.noreply.github.com>
2021-12-07 15:19:26 +0100

    work iv and pl

--- Masterarbeit/Python Programme/IV/IV_Analyse.py
+++ Masterarbeit/Python Programme/IV/IV_Analyse.py
@@ -8,16 +8,12 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import tkinter as tk
 from tkinter import filedialog
 import matplotlib as mpl
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 plt.rcParams['font.family'] = "Arial"
 plt.rcParams['font.size'] = 14
 root = tk.Tk()
 root.withdraw()
-
-image_01 = filedialog.askopenfilename()
-#print(image_01)
-IV_directory = "/".join(image_01.split("/")[:-1])
-#print(IV_directory)
 
 class IV_Analyse():
     def __init__(self, path):
@@ -47,32 +43,80 @@ class IV_Analyse():
         cell_number = cell_number.split('.')[0]
         return str(cell_number)
 
-# create the figure
-width = 12/2.54
-fig = plt.figure(figsize=(width, width))
-ax = fig.add_subplot(111)
-#ax.set_aspect("equal")
-#ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
-ax.set_xlabel(r"Spannung $\mathit{U}$ (V)")
-ax.set_ylabel(r"Stromdichte $\mathit{J}$ (mA/cm$^2$)")
-ax.yaxis.set_ticks_position('both')
-ax.xaxis.set_ticks_position('both')
-#ax.set_yscale('log')
-ax.axhline(0, color='black', linewidth=1)
-ax.axvline(0, color='black', linewidth=1)
-print(IV_directory)
-Substrate_name = IV_directory.split('/')[-1]
-mode = 'lgt'
-for path in glob.iglob(IV_directory+"/*."+mode):
-    #print(path)
-    IV_plt = IV_Analyse(path)
-    volt, curr = IV_plt.show_IV()
-    ax.plot(volt, curr, linestyle='None', marker='.', markersize=5, label="Z "+IV_plt.return_label())
-ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
-lgnd = plt.legend(loc="upper left", scatterpoints=1, fontsize=10)
+#image_01 = filedialog.askopenfilename()
+#print(image_01)
+#IV_directory = "/".join(image_01.split("/")[:-2])
+#print("current working on:",IV_directory)
+List_Substrates = [
+    "G1963",
+    "G1965",
+    "G1967",
+    "G1968",
+    "G1971",
+    "G1974"
+]
+
+for substrate in ["G1963", "G1974"]:
+    IV_directory = "C:/Users/j.oberroehrmann/Documents/Messdaten/Loana_Lukas/P3T_063/"+substrate
+    # create the figure
+    width = 14/2.54
+    fig = plt.figure(figsize=(2*width, width))
+    ax = fig.add_subplot(121)
+    ax_inset = inset_axes(ax, width="30%", height="30%", loc=(9))
+    ax2 = fig.add_subplot(122)
+    ax2_inset = inset_axes(ax2, width="30%", height="30%", loc=(9))
+
+    #ax.set_aspect("equal")
+    #ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+    ax.set_xlabel(r"Spannung $\mathit{U}$ (V)")
+    ax.set_ylabel(r"Stromdichte $\mathit{J}$ (mA/cm$^2$)")
+    ax_inset.set_xlabel(r"$\mathit{U}$ (V)")
+    ax_inset.set_ylabel(r"$\mathit{J}$ (mA/cm$^2$)")
+    ax2.set_xlabel(r"Spannung $\mathit{U}$ (V)")
+    ax2.set_ylabel(r"Stromdichte $\mathit{J}$ (mA/cm$^2$)")
+    ax2_inset.set_xlabel(r"$\mathit{U}$ (V)")
+    ax2_inset.set_ylabel(r"$\mathit{J}$ (mA/cm$^2$)")
+    
+    ax.yaxis.set_ticks_position('both')
+    ax.xaxis.set_ticks_position('both')
+    ax2.yaxis.set_ticks_position('both')
+    ax2.xaxis.set_ticks_position('both')
+    #ax.set_yscale('log')
+    ax.axhline(0, color='black', linewidth=1)
+    ax.axvline(0, color='black', linewidth=1)
+    ax2.axhline(0, color='black', linewidth=1)
+    ax2.axvline(0, color='black', linewidth=1)
+    print(IV_directory)
+    Substrate_name = IV_directory.split('/')[-1]
+    print("Substrate name:", Substrate_name)
+    Mode = IV_directory.split('/')[-1]
+    dict_mode = {"Hell":'lgt', "Dunkel":'drk'}
+    for path in glob.iglob(IV_directory+"/Dunkel"+"/*.drk"):#+dict_mode[Mode]):
+        print(path)
+        IV_plt = IV_Analyse(path)
+        volt, curr = IV_plt.show_IV()
+        ax.plot(volt, curr, linestyle='None', marker='.', markersize=5, label="Zelle "+IV_plt.return_label())
+        ax_inset.plot(volt, curr, linestyle='None', marker='.', markersize=5, label="Zelle "+IV_plt.return_label())
+    for path in glob.iglob(IV_directory+"/Hell"+"/*.lgt"):#+dict_mode[Mode]):
+        print(path)
+        IV_plt = IV_Analyse(path)
+        volt, curr = IV_plt.show_IV()
+        ax2_inset.plot(volt, curr, linestyle='None', marker='.', markersize=5, label="Zelle " + IV_plt.return_label())
+        ax2.plot(volt, curr, linestyle='None', marker='.', markersize=5, label="Zelle "+IV_plt.return_label())
+
+    ax.set_yscale('log')
+    ax2.set_ylim(-3, 1)
+    ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+    ax2.set_aspect(1.0/ax2.get_data_ratio(), adjustable='box')
+    #lgnd = plt.legend(loc="upper left", scatterpoints=1, fontsize=10)
+    ax.set_title("Dunkelkennlinie")
+    ax2.set_title("Hellkennlinie")
+
+    #ax_inset.set_ylim(-0.01,0.01)
+    #ax_inset.set_yscale('log')
 
 
-
-plt.tight_layout()
-plt.savefig("C:/Users/bomml/Documents/Python/IV/Pictures/"+Substrate_name+"_"+mode+".svg", transparent='true')
-#plt.show()
+    ax2.legend(loc="lower right")
+    plt.tight_layout()
+    plt.savefig(IV_directory+"/"+Substrate_name+".svg", transparent='true')
+    #plt.show()
--- Masterarbeit/Python Programme/PL/Read_PL_data.py
+++ Masterarbeit/Python Programme/PL/Read_PL_data.py
@@ -6,23 +6,134 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import pandas as pd
+from pathlib import Path
 
 plt.style.use('../PL/plot.mplstyle')
+figsize = 14/2.54 # 14 cm in inches
 root = tk.Tk()
 root.withdraw()
 import codecs
-blockSize = 1048576
 #image_01 = filedialog.askopenfilename()
-image_01 = "G1963_Zelle4_aufPero_01.txt"
+#image_01 = "G1963_Zelle4_aufPero_01.txt"
 
-with open(image_01, 'r') as file:
-    data = file.readlines()
+class PL_Spectrum():
+    def __init__(self, path):
+        print(f"Now working with file: {path}.")
+        self.path = str(path)
+        self.get_filename()
+        self.get_labelname()
+        # read the data from the file to the dataframe
+        self.read_data()
+        # get characteristics from spectrum
+        self.get_characteristics()
 
-wavelengths = [float(item.strip().split("\t")[0]) for item in data]
-Spectrum = [float(item.strip().split("\t")[1]) for item in data]
-fig = plt.figure()
+    def read_data(self):
+        with open(self.path, 'r') as file:
+            data = file.readlines()
+
+        Energies = [float(item.strip().split("\t")[0]) for item in data]
+        Spectrum = [float(item.strip().split("\t")[1]) for item in data]
+        # create a dict of both arrays
+        dict_spectrum = {"Energie (eV)": Energies, "Intensity (counts)": Spectrum}
+        # dict to DataFrame
+        dataframe_spectrum = pd.DataFrame()
+        dataframe_spectrum = dataframe_spectrum.from_dict(dict_spectrum)
+        dataframe_spectrum["Intensity rolling mean (counts)"] = dataframe_spectrum["Intensity (counts)"].rolling(200,center=True).mean()
+        self.dataframe_spectrum = dataframe_spectrum.set_index(dataframe_spectrum["Energie (eV)"])
+
+    def get_filename(self):
+        #print(self.path)
+        name = self.path.split("\\")[-1].strip('.txt')
+        #print(name)
+        self.name = name
+
+    def get_labelname(self):
+        #print(self.path)
+        label = self.path.strip('.txt').split("\\")[-1].split("_")[-2:]
+        #print(name)
+        self.label_name = " ".join(label)
+
+    def set_name(self, manual_name):
+        self.name = manual_name
+
+    def get_characteristics(self):
+        # make Dataframe with characteristics: Maximum_Energie, Maximum_Intensity, FWHM
+        df_index = {"Cell": ["Maximum Energie (eV)", "Maximum Intensity (counts)"]}
+        Maximum_index = self.dataframe_spectrum["Intensity rolling mean (counts)"].idxmax()
+        Maximum_spectrum = self.dataframe_spectrum.loc[Maximum_index]
+        #print(Maximum_spectrum)
+        self.Maximum_spectrum = Maximum_spectrum
+
+    def set_linestyle(self):
+        """
+        check if the filename has an "auf Defekt", if yes set linestyle to dotted, else set to solid
+        :return:
+        """
+        if self.path.find("aufDefekt")==-1:
+            return "solid"
+        else:
+            return "dashed"
+    def plot_spectrum_smoothed(self, ax):
+        linestyle = self.set_linestyle()
+        ax.plot(self.dataframe_spectrum["Intensity rolling mean (counts)"], linestyle=linestyle, label=self.label_name)
+
+    def plot_spectrum_raw(self, ax):
+        ax.plot(self.dataframe_spectrum["Intensity (counts)"], label=self.label_name+" raw")
+
+    def add_char_to_dataframe(self, df):
+        """
+        gets a dataframe df and adds the PL characteristics to it.
+        :return: dataframe df with characs
+
+        """
+        dict = {self.label_name : self.Maximum_spectrum}
+        frame = pd.DataFrame.from_dict(dict)
+        return pd.concat([df, frame],axis=1)
+dir = "C:/Users/j.oberroehrmann/Documents/Messdaten/PL/P3T_063/20211206_G1967/"
+dir_add = "G1967_Zelle1"
+PL_files = []
+# search directory and subdirectories with: for path in Path(dir).rglob("*.txt"):
+for path in glob.iglob(dir+dir_add+"*.txt"):
+    PL_files.append(path)
+
+print(PL_files)
+fig = plt.figure(figsize=(figsize, 0.5*figsize))
 ax = fig.add_subplot(111)
-ax.plot(Spectrum)
 
+PL_characteristics = pd.DataFrame()
+
+for file in PL_files:
+    # init object
+    PL_01 = PL_Spectrum(file)
+    # add smoothed line to plot
+    PL_01.plot_spectrum_smoothed(ax)
+    # add characteristics to the dataframe
+    PL_characteristics = PL_01.add_char_to_dataframe(PL_characteristics)
+ax.set_ylabel("PL Intensität (counts)")
+ax.set_xlabel("Energie (eV)")
+ax.set_xlim(1.3, 1.9)
+#ax.set_yscale('log')
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
-plt.show()
+plt.tight_layout()
+plt.savefig(dir+"/PL_spectrum.svg", transparent = True)
+#plt.show()
+PL_characteristics = PL_characteristics.transpose()
+PL_characteristics.to_excel(dir+"/PL_characteristics.xlsx")
+fig = plt.figure(figsize=(figsize, 0.6*figsize))
+ax = fig.add_subplot(121)
+ax.plot(PL_characteristics["Energie (eV)"], linestyle="None", marker=".")
+ax.set_ylabel("Energie (eV)")
+plt.xticks(rotation=45, ha='right')
+xlabel = ax.xaxis.get_label()
+ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+ax = fig.add_subplot(122)
+ax.plot(PL_characteristics["Intensity rolling mean (counts)"], linestyle="None", marker=".")
+ax.set_ylabel("Intensity (counts)")
+xlabel = ax.xaxis.get_label()
+ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.savefig(dir+ "/PL_Characteristics.svg", transparent=True)
+#plt.show()
